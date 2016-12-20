dfd37e439d5c81ea4046535624fa82f49e569000
Sam Stiles <samuel.dev.stiles@gmail.com>
2016-12-20 19:02:43 -0400

    More setup

--- src/client_sdk/core.cljs
+++ src/client_sdk/core.cljs
@@ -1,7 +1,20 @@
 (ns client-sdk.core
-  (:require-macros [cljs.core.async.macros :refer [go-loop]])
+  (:require-macros [cljs.core.async.macros :refer [go-loop go]])
   (:require [cljs.core.async :as a]
             [auth-sdk.core :as auth]
             [presence-sdk.core :as pres]))
 
 (enable-console-print!)
+
+(defonce sdk-state
+  (atom {}))
+
+(defn init-module [sdk-state module-name module-state]
+  (assoc sdk-state module-name module-state))
+
+(defn ^:export init []
+  (-> @sdk-state
+      (init-module :auth (auth/init))))
+
+(defn login)
+(defn change-presence-state)
