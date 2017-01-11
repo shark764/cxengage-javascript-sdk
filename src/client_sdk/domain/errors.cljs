63036035d0d0a5877d0f5ef1f59b8dab01c78d18
Sam Stiles <samuel.dev.stiles@gmail.com>
2017-01-11 13:49:49 -0400

    Return full response format

--- src/client_sdk/domain/errors.cljs
+++ src/client_sdk/domain/errors.cljs
@@ -3,5 +3,6 @@
 (defn invalid-params-err
   ([] (invalid-params-err nil))
   ([msg]
-   {:error-code 15000
-    :error-msg (or msg "Invalid arguments passed to SDK function.")}))
+   {:response nil
+    :error {:code 15000
+            :msg (or msg "Invalid arguments passed to SDK function.")}}))
