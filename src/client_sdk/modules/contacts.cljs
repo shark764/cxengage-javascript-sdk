7fdde09aab182aae2d54c911939b35e47900f137
Janet Carr <jcarr@liveops.com>
2017-01-23 15:15:27 -0400

    Finished unit tests, and fixed up some functions

--- src/client_sdk/modules/contacts.cljs
+++ src/client_sdk/modules/contacts.cljs
@@ -13,11 +13,10 @@
   (let [request-map {:url url
                      :method method
                      :resp-chan resp}]
-    (when body
-      (assoc request-map :body body))
-    (when token
-      (assoc request-map :token token))
-    (u/api-request request-map)
+    (cond-> request-map 
+            body (assoc :body body)
+            token (assoc :token token)
+            true (u/api-request))
     (go
       (a/>! resp-chan (:result (a/<! resp))))))
 
@@ -74,16 +73,19 @@
 
 (defn delete-contact
   [message]
-  (let [{:keys [token contact-id tenant-id resp-chan attributes]} message
+  (let [{:keys [token contact-id tenant-id resp-chan]} message
         resp (a/promise-chan)
-        request-map {:url (u/api-url (:env @module-state) (str "/tenants/" tenant-id "/contacts/" contact-id))
-                     :method :delete
-                     :token token
-                     :resp-chan resp}]
-    (u/api-request request-map)
-    (go
-      (when (= (:status (a/<! resp)) 201)
-        (a/>! resp-chan true)))))
+        url (u/api-url (:env @module-state) (str "/tenants/" tenant-id "/contacts/" contact-id))
+        method :delete]
+    (contact-request url nil method token resp resp-chan)))
+
+(defn get-attribute
+  [message]
+  (let [{:keys [token tenant-id attribute-id resp-chan]} message
+        resp (a/promise-chan)
+        url (u/api-url (:env @module-state) (str "/tenants/" tenant-id "/contacts/attributes/" attribute-id))
+        method :get]
+    (contact-request url nil method token resp resp-chan)))
 
 (defn list-attributes
   [message]
@@ -95,20 +97,110 @@
 
 (defn create-attribute
   [message]
-  (let [{:keys [token tenant-id resp-chan attribute]} message
+  (let [{:keys [token tenant-id resp-chan label object-name type mandatory default]} message
         resp (a/promise-chan)
         url (u/api-url (:env @module-state) (str "/tenants/" tenant-id "/contacts/attributes"))
-        body attribute
+        body (cond-> {:label label
+                      :objectName object-name
+                      :type type}
+                     (not (nil? mandatory)) (assoc :mandatory mandatory)
+                     default (assoc :default default))
+        method :post] 
+    (contact-request url body method token resp resp-chan)))
+
+(defn update-attribute
+  [message]
+  (let [{:keys [token tenant-id attribute-id resp-chan label mandatory default]} message
+        resp (a/promise-chan)
+        url (u/api-url (:env @module-state) (str "/tenants/" tenant-id "/contacts/attributes/" attribute-id))
+        body (cond-> {}
+                     label (assoc :label label)
+                     default (assoc :default default)
+                     (not (nil? mandatory)) (assoc :mandatory mandatory))
+        method :put]
+    (contact-request url body method token resp resp-chan)))
+
+(defn delete-attribute
+  [message]
+  (let [{:keys [token tenant-id attribute-id resp-chan]} message
+        resp (a/promise-chan)
+        url (u/api-url (:env @module-state) (str "/tenants/" tenant-id "/contacts/attributes/" attribute-id))
+        method :delete]
+    (contact-request url nil method token resp resp-chan)))
+
+(defn get-layout
+  [message]
+  (let [{:keys [token tenant-id layout-id resp-chan]} message
+        resp (a/promise-chan)
+        url (u/api-url (:env @module-state) (str "/tenants/" tenant-id "/contacts/layouts/" layout-id))
+        method :get]
+    (contact-request url nil method token resp resp-chan)))
+
+(defn list-layouts
+  [message]
+  (let [{:keys [token tenant-id resp-chan]} message
+        resp (a/promise-chan)
+        url (u/api-url (:env @module-state) (str "/tenants/" tenant-id "/contacts/layouts"))
+        method :get]
+    (contact-request url nil method token resp resp-chan)))
+
+(defn create-layout
+  [message]
+  (let [{:keys [token tenant-id resp-chan labels attributes name description active]} message
+        resp (a/promise-chan)
+        url (u/api-url (:env @module-state) (str "/tenants/" tenant-id "/contacts/layouts"))
+        layout (->> labels
+                    (reduce-kv (fn [acc k v]
+                                 (conj acc {:label v})) [])
+                    (map #(assoc %2 :attributes %1) attributes))
+        body (cond-> {:layout layout
+                      :name name}
+                     description (assoc :description description)
+                     (not (nil? active)) (assoc :active active))
         method :post]
     (contact-request url body method token resp resp-chan)))
 
+(defn update-layout
+  [message]
+  (let [{:keys [token tenant-id layout-id resp-chan labels attributes name description active]} message
+        resp (a/promise-chan)
+        url (u/api-url (:env @module-state) (str "/tenants/" tenant-id "/contacts/layouts/" layout-id))
+        layout (->> labels
+                    (reduce-kv (fn [acc k v]
+                                 (conj acc {:label v})) [])
+                    (map #(assoc %2 :attributes %1) attributes))
+        body (cond-> {}
+                     (and labels attributes) (assoc :layout layout)
+                     description (assoc :description description)
+                     name (assoc :name name)
+                     (not (nil? active)) (assoc :active active))
+        method :put]
+    (contact-request url body method token resp resp-chan)))
+
+(defn delete-layout
+  [message]
+  (let [{:keys [token tenant-id layout-id resp-chan]} message
+        resp (a/promise-chan)
+        url (u/api-url (:env @module-state) (str "/tenants/" tenant-id "/contacts/layouts/" layout-id))
+        method :delete]
+    (contact-request url nil method token resp resp-chan)))
+
 (defn module-router [message]
   (let [handling-fn (case (:type message)
-                      :CONTACTS/GET get-contact
-                      :CONTACTS/CREATE create-contact
-                      :CONTACTS/LIST list-contacts
-                      :CONTACTS/UPDATE update-contact
-                      :CONTACTS/DELETE delete-contact
+                      :CONTACTS/GET_CONTACT get-contact
+                      :CONTACTS/CREATE_CONTACT create-contact
+                      :CONTACTS/LIST_CONTACT list-contacts
+                      :CONTACTS/UPDATE_CONTACT update-contact
+                      :CONTACTS/DELETE_CONTACT delete-contact
+                      :CONTACTS/GET_ATTRIBUTE get-attribute
+                      :CONTACTS/LIST_ATTRIBUTES list-attributes
+                      :CONTACTS/CREATE_ATTRIBUTE create-attribute
+                      :CONTACTS/UPDATE_ATTRIBUTE update-attribute
+                      :CONTACTS/GET_LAYOUT get-layout
+                      :CONTACTS/LIST_LAYOUTS list-layouts
+                      :CONTACTS/CREATE_LAYOUT create-layout
+                      :CONTACTS/UPDATE_LAYOUT update-layout
+                      :CONTACTS/DELETE_LAYOUT delete-layout
                       nil)]
     (if handling-fn
       (handling-fn message)
@@ -120,7 +212,7 @@
 
 (defn init [env]
   (log :info "Initializing SDK module: Contacts")
-  (swap! module-state :env env)
+  (swap! module-state assoc :env env)
   (let [module-inputs< (a/chan 1024)
         module-shutdown< (a/chan 1024)]
     (u/start-simple-consumer! module-inputs< module-router)
