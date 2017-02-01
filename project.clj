f23810b3813a12528d8d9f8d85e770bc36846a4b
Sam Stiles <samuel.dev.stiles@gmail.com>
2017-02-01 14:38:08 -0400

    Release 1.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.serenova/client-sdk-core "1.1.1-SNAPSHOT"
+(defproject org.serenova/client-sdk-core "1.1.1"
   :description "Client SDK Core"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
