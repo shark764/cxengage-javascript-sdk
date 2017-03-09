9cf7c481842971f2ab37554e03845acfd776ab9c
Sam Stiles <samuel.dev.stiles@gmail.com>
2017-03-09 15:36:35 -0400

    Fix error ns when advanced compiled

--- src/cljs/cxengage_javascript_sdk/domain/errors.cljs
+++ src/cljs/cxengage_javascript_sdk/domain/errors.cljs
@@ -1,17 +1,17 @@
 (ns cxengage-javascript-sdk.domain.errors)
 
-(defn error [error]
+(defn error-output [error]
   (js/console.error ">>>ERROR<<<" (clj->js (:error error)) (clj->js (:data error)))
   error)
 
-(defn wrong-number-of-args-error [] (error {:code 1000 :error "Incorrect number of arguments passed to SDK fn."}))
-(defn missing-required-permissions-error [] (error {:code 1003 :error "Missing required permissions"}))
+(defn wrong-number-of-args-error [] (error-output {:code 1000 :error "Incorrect number of arguments passed to SDK fn."}))
+(defn missing-required-permissions-error [] (error-output {:code 1003 :error "Missing required permissions"}))
 (defn invalid-args-error [spec-explanation]
-  (error {:code 1001 :error "Invalid arguments passed to SDK fn." :data spec-explanation}))
-(defn api-error [error] (merge (error {:code 1002 :error "API returned an error."} error)))
+  (error-output {:code 1001 :error "Invalid arguments passed to SDK fn." :data spec-explanation}))
+(defn api-error [error] (merge (error-output {:code 1002 :error "API returned an error."}) error))
 (defn no-entity-found-for-specified-id [entity entity-id]
-  (error {:code 1004 :error (str "No " entity " found by that ID") :data entity-id}))
+  (error-output {:code 1004 :error (str "No " entity " found by that ID") :data entity-id}))
 (defn no-microphone-access-error [err]
-  (error {:code 7000 :error (str "No access to microphone: " (.-message err))}))
+  (error-output {:code 7000 :error (str "No access to microphone: " (.-message err))}))
 (defn not-a-valid-extension [] {:code 5000 :error "that isn't a valid extension."})
 (defn invalid-logging-level-specified-error [] "Invalid logging level specified.")
