ac130c6e7e86af930742e1e6df8138c7139d559b
Sam Stiles <samuel.dev.stiles@gmail.com>
2017-04-24 22:11:20 -0300

    :duck: :duck: :duck: :duck: :duck: :duck: :duck: :duck:

--- src/dev_cljs/dev/dev.cljs
+++ src/dev_cljs/dev/dev.cljs
@@ -1,5 +1,7 @@
 (ns dev.dev
   (:require [devtools.core :as devtools]))
 
+(js/console.info "                _      _      _\n              >(.)__ <(.)__ =(.)__\n               (___/  (___/  (___/\n\n              we are the ducklets of\n           safe code, your code will run\n            without errors, but only if\n           you say \"compile well duckos\"\n\n")
+
 (devtools/install! [:formatters :hints])
 (enable-console-print!)
