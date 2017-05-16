bbce53009ea6a1ec487e01020dda7902a0738b31
Janet A Carr <jcarr@serenova.com>
2017-05-16 16:07:10 -0300

    CXV1-9281 Nextify Contacts Module w/ Unit tests

--- src/cljs/cxengage_javascript_sdk/modules/contacts.cljs
+++ src/cljs/cxengage_javascript_sdk/modules/contacts.cljs
@@ -1,48 +1,12 @@
 (ns cxengage-javascript-sdk.modules.contacts
-  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require-macros [cxengage-javascript-sdk.macros :refer [def-sdk-fn]])
   (:require [cljs.core.async :as a]
             [cljs.spec :as s]
             [cxengage-javascript-sdk.domain.protocols :as pr]
-            [cxengage-javascript-sdk.domain.errors :as e]
+            [cxengage-javascript-sdk.domain.rest-requests :as rest]
             [cxengage-javascript-sdk.pubsub :as p]
             [cxengage-javascript-sdk.interop-helpers :as ih]
-            [cxengage-javascript-sdk.internal-utils :as iu]
-            [cxengage-javascript-sdk.domain.specs :as specs]
-            [cxengage-javascript-sdk.state :as state]))
-
-(defn contact-request
-  ([url body method params topic-key spec preserve-casing?]
-   (contact-request url body method params topic-key spec preserve-casing? nil))
-  ([url body method params topic-key spec preserve-casing? query]
-   (let [{:keys [callback]} params
-         urls {:single-contact-url "tenants/:tenant-id/contacts/:contact-id"
-               :multiple-contact-url "tenants/:tenant-id/contacts"
-               :merge-contacts-url "tenants/:tenant-id/contacts/merge"
-               :multiple-attribute-url "tenants/:tenant-id/contacts/attributes"
-               :multiple-layout-url "tenants/:tenant-id/contacts/layouts"
-               :single-layout-url "tenants/:tenant-id/contacts/layouts/:layout-id"}
-         request-url (iu/api-url
-                      (get urls (:base url))
-                      (:params url))
-         topic (p/get-topic topic-key)]
-     (if-not (s/valid? spec params)
-       (p/publish {:topics topic
-                   :error (e/args-failed-spec-err)
-                   :callback callback})
-       (do (go (let [response (a/<! (contact-request request-url body method preserve-casing?))
-                     {:keys [status api-response]} response
-                     {:keys [result]} api-response]
-                 (when (= status 200)
-                   (p/publish {:topics topic
-                               :response result
-                               :callback callback} true))))
-           nil))))
-  ([url body method preserve-casing?]
-   (let [request-map {:url url
-                      :method method}]
-     (cond-> request-map
-       body (assoc :body body)
-       true (iu/api-request preserve-casing?)))))
+            [cxengage-javascript-sdk.domain.specs :as specs]))
 
 (defn get-query-str
   [query]
@@ -53,207 +17,217 @@
     (clojure.string/join queryv)))
 
 
+;; -------------------------------------------------------------------------- ;;
+;; Cxengage.contacts.get({contactId: "{{contact-id}}" });
+;; -------------------------------------------------------------------------- ;;
+
 (s/def ::get-contact-params
-  (s/keys :req-un [::specs/contactId]
+  (s/keys :req-un [::specs/contact-id]
           :opt-un [::specs/callback]))
 
-(defn get-contact
-  ([] (e/wrong-number-of-sdk-fn-args-err))
-  ([params & others]
-   (if-not (fn? (js->clj (first others)))
-     (e/wrong-number-of-sdk-fn-args-err)
-     (get-contact (merge (ih/extract-params params true) {:callback (first others)}))))
-  ([params]
-   (let [{:keys [contactId callback] :as params} (ih/extract-params params true)
-         url {:base :single-contact-url
-              :params {:tenant-id (state/get-active-tenant-id)
-                       :contact-id contactId}}
-         method :get]
-     (contact-request url nil method params :get-contact ::get-contact-params true))))
+(def-sdk-fn get-contact
+  {:validation ::get-contact-params
+   :topic-key :get-contact}
+  [params]
+  (let [{:keys [contact-id callback topic]} params
+        {:keys [api-response status]} (a/<! (rest/get-contact-request contact-id))
+        retrieved-contact (:result api-response)]
+    (when (= status 200)
+      (p/publish {:topics topic
+                  :response retrieved-contact
+                  :callback callback} true))))
+
+;; -------------------------------------------------------------------------- ;;
+;; Cxengage.contacts.getAll();
+;; -------------------------------------------------------------------------- ;;
 
 (s/def ::get-contacts-params
   (s/keys :req-un []
           :opt-un [::specs/callback]))
 
-(defn get-contacts
-  ([]
-   (get-contacts {}))
-  ([params & others]
-   (if-not (fn? (js->clj (first others)))
-     (e/wrong-number-of-sdk-fn-args-err)
-     (get-contacts (merge (ih/extract-params params true) {:callback (first others)}))))
-  ([params]
-   (let [params (if (fn? params) {:callback params} params)
-         {:keys [callback] :as params} (ih/extract-params params)
-         url {:base :multiple-contact-url
-              :params {:tenant-id (state/get-active-tenant-id)}}
-         method :get]
-     (contact-request url nil method params :get-contacts ::get-contacts-params true))))
+(def-sdk-fn get-contacts
+  {:validation ::get-contacts-params
+   :topic-key :get-contacts}
+  [params]
+  (let [{:keys [callback topic]} params
+        {:keys [api-response status]} (a/<! (rest/get-contacts-request))
+        retrieved-contacts (:result api-response)]
+    (when (= status 200)
+      (p/publish {:topics topic
+                  :response retrieved-contacts
+                  :callback callback} true))))
+
+;; -------------------------------------------------------------------------- ;;
+;; // Search on a specified attribute
+;; CxEngage.contacts.search({query: {name: "Serenova"}});
+;;
+;; // Search all attributes for value by using the keyword q
+;; CxEngage.contacts.search({query: {q: "Serenova"}});
+;; -------------------------------------------------------------------------- ;;
 
 (s/def ::search-contacts-params
   (s/keys :req-un [::specs/query]
           :opt-un [::specs/callback]))
 
-(defn search-contacts
-  ([] (e/wrong-number-of-sdk-fn-args-err))
-  ([params & others]
-   (if-not (fn? (js->clj (first others)))
-     (e/wrong-number-of-sdk-fn-args-err)
-     (search-contacts (merge (ih/extract-params params true) {:callback (first others)}))))
-  ([params]
-   (let [{:keys [query callback] :as params} (ih/extract-params params true)
-         url {:base :multiple-contact-url
-              :params {:tenant-id (state/get-active-tenant-id)}}
-         method :get
-         query (get-query-str query)]
-     (contact-request url nil method params :search-contacts ::search-contacts-params true query))))
+(def-sdk-fn search-contacts
+  {:validation ::search-contacts-params
+   :topic-key :search-contacts
+   :preserve-casing? true}
+  [params]
+  (let [{:keys [query callback topic]} params
+        query (get-query-str query)
+        {:keys [api-response status]} (a/<! (rest/search-contacts-request query))
+        found-contacts (:result api-response)]
+    (when (= status 200)
+      (p/publish {:topics topic
+                  :response found-contacts
+                  :callback callback} true))))
+
+;; -------------------------------------------------------------------------- ;;
+;; CxEngage.contacts.create({attributes: {name: "Serenova, LLC."}});
+;; -------------------------------------------------------------------------- ;;
 
 (s/def ::create-contact-params
   (s/keys :req-un [::specs/attributes]
           :opt-un [::specs/callback]))
 
-(defn create-contact
-  ([] (e/wrong-number-of-sdk-fn-args-err))
-  ([params & others]
-   (if-not (fn? (js->clj (first others)))
-     (e/wrong-number-of-sdk-fn-args-err)
-     (create-contact (merge (ih/extract-params params true) {:callback (first others)}))))
-  ([params]
-   (let [{:keys [attributes callback] :as params} (ih/extract-params params true)
-         url {:base :multiple-contact-url
-              :params {:tenant-id (state/get-active-tenant-id)}}
-         method :post
-         body {:attributes attributes}]
-     (contact-request url body method params :create-contact ::create-contact-params true))))
+(def-sdk-fn create-contact
+  {:validation ::create-contact-params
+   :topic-key :create-contact
+   :preserve-casing? true}
+  [params]
+  (let [{:keys [attributes callback topic]} params
+        {:keys [api-response status]} (a/<! (rest/create-contact-request {:attributes attributes}))
+        created-contact (:result api-response)]
+    (when (= status 200)
+      (p/publish {:topics topic
+                  :response created-contact
+                  :callback callback} true))))
+
+;; ----------------------------------------------------------------------------------------------- ;;
+;; CxEngage.contacts.update({contactId: "{{contact-id}}", attributes: {name: "Serenova, LLC."}});
+;; ----------------------------------------------------------------------------------------------- ;;
 
 (s/def ::update-contact-params
   (s/keys :req-un [::specs/contactId
                    ::specs/attributes]
           :opt-un [::specs/callback]))
 
-(defn update-contact
-  ([] (e/wrong-number-of-sdk-fn-args-err))
-  ([params & others]
-   (if-not (fn? (js->clj (first others)))
-     (e/wrong-number-of-sdk-fn-args-err)
-     (update-contact (merge (ih/extract-params params true) {:callback (first others)}))))
-  ([params]
-   (let [{:keys [attributes contactId callback] :as params} (ih/extract-params params true)
-         url {:base :single-contact-url
-              :params {:tenant-id (state/get-active-tenant-id)
-                       :contact-id contactId}}
-         method :put
-         body {:attributes attributes}]
-     (contact-request url body method params :update-contact ::update-contact-params true))))
+(def-sdk-fn update-contact
+  {:validation ::update-contact-params
+   :topic-key :update-contact
+   :preserve-casing? true}
+  [params]
+  (let [{:keys [attributes contactId callback topic]} params
+        {:keys [api-response status]} (a/<! (rest/update-contact-request contactId {:attributes attributes}))
+        updated-contact (:result api-response)]
+    (when (= status 200)
+      (p/publish {:topics topic
+                  :response updated-contact
+                  :callback callback}))))
+
+;; -------------------------------------------------------------------------- ;;
+;; CxEngage.contacts.delete({contactId: "{{contact-id}}"});
+;; -------------------------------------------------------------------------- ;;
 
 (s/def ::delete-contact-params
-  (s/keys :req-un [::specs/contactId]
+  (s/keys :req-un [::specs/contact-id]
           :opt-un [::specs/callback]))
 
-(defn delete-contact
-  ([] (e/wrong-number-of-sdk-fn-args-err))
-  ([params & others]
-   (if-not (fn? (js->clj (first others)))
-     (e/wrong-number-of-sdk-fn-args-err)
-     (delete-contact (merge (ih/extract-params params true) {:callback (first others)}))))
-  ([params]
-   (let [{:keys [contactId callback] :as params} (ih/extract-params params true)
-         url {:base :single-contact-url
-              :params {:tenant-id (state/get-active-tenant-id)
-                       :contact-id contactId}}
-         method :delete]
-     (contact-request url nil method params :delete-contact ::delete-contact-params true))))
+(def-sdk-fn delete-contact
+  {:validation ::delete-contact-params
+   :topic-key :delete-contact}
+  [params]
+  (let [{:keys [contact-id callback topic]} params
+        {:keys [api-response status]} (a/<! (rest/delete-contact-request contact-id))
+        deleted-contact? (:result api-response)]
+    (when (= status 200)
+      (p/publish {:topics topic
+                  :response deleted-contact?
+                  :callback callback}) true)))
+
+;; ----------------------------------------------------------------------------------------------------------------- ;;
+;; CxEngage.contacts.merge({contactIds: ["{{contact-id}}", "{{contact-id}}"], attributes: {name: "Serenova, LLC."}});
+;; ----------------------------------------------------------------------------------------------------------------- ;;
 
 (s/def ::merge-contacts-params
   (s/keys :req-un [::specs/contactIds
                    ::specs/attributes]
           :opt-un [::specs/callback]))
 
-(defn merge-contacts
-  ([] (e/wrong-number-of-sdk-fn-args-err))
-  ([params & others]
-   (if-not (fn? (js->clj (first others)))
-     (e/wrong-number-of-sdk-fn-args-err)
-     (merge-contacts (merge (ih/extract-params params true) {:callback (first others)}))))
-  ([params]
-   (let [{:keys [contactIds attributes callback] :as params} (ih/extract-params params true)
-         url {:base :merge-contacts-url
-              :params {:tenant-id (state/get-active-tenant-id)}}
-         method :post
-         body {:attributes attributes
-               :contactIds contactIds}]
-     (contact-request url body method params :merge-contacts ::merge-contacts-params true))))
+(def-sdk-fn merge-contacts
+  {:validation ::merge-contacts-params
+   :topic-key :merge-contacts
+   :preserve-casing? true}
+  [params]
+  (let [{:keys [contactIds attributes callback topic]} params
+        {:keys [api-response status]} (a/<! (rest/merge-contact-request {:contactIds contactIds
+                                                                         :attributes attributes}))
+        merged-contact (:result api-response)]
+    (when (= status 200)
+      (p/publish {:topics topic
+                  :response merged-contact
+                  :callback callback} true))))
+
+;; -------------------------------------------------------------------------- ;;
+;; CxEngage.contacts.listAttributes();
+;; -------------------------------------------------------------------------- ;;
 
 (s/def ::list-attributes-params
   (s/keys :req-un []
           :opt-un [::specs/callback]))
 
-(defn list-attributes
-  ([]
-   (list-attributes {}))
-  ([params & others]
-   (if-not (fn? (js->clj (first others)))
-     (e/wrong-number-of-sdk-fn-args-err)
-     (list-attributes (merge (ih/extract-params params true) {:callback (first others)}))))
-  ([params]
-   (let [params (if (fn? params) {:callback params} params)
-         {:keys [callback] :as params} (ih/extract-params params true)
-         method :get
-         request-url (iu/api-url
-                      "tenants/:tenant-id/contacts/attributes"
-                      {:tenant-id (state/get-active-tenant-id)})
-         topic (p/get-topic :list-attributes)]
-     (if-not (s/valid? ::list-attributes-params params)
-       (p/publish {:topics topic
-                   :error (e/args-failed-spec-err)
-                   :callback callback})
-       (go (let [request-map {:url request-url
-                              :method method}
-                 response (a/<! (iu/api-request request-map true))
-                 {:keys [status api-response]} response
-                 {:keys [result]} api-response]
-             (when (= status 200)
-               (p/publish {:topics topic
-                           :response result
-                           :callback callback} true)))
-           nil)))))
+(def-sdk-fn list-attributes
+  {:validation ::list-attributes-params
+   :topic-key :list-attributes}
+  [params]
+  (let [{:keys [callback topic]} params
+        {:keys [api-response status]} (a/<! (rest/list-attributes-request))
+        retrieved-attributes (:result api-response)]
+    (when (= status 200)
+      (p/publish {:topics topic
+                  :response retrieved-attributes
+                  :callback callback} true))))
+
+;; -------------------------------------------------------------------------- ;;
+;; CxEngage.contacts.getLayout({layoutId: "{{layout-id}}"});
+;; -------------------------------------------------------------------------- ;;
 
 (s/def ::get-layout-params
-  (s/keys :req-un [::specs/layoutId]
+  (s/keys :req-un [::specs/layout-id]
           :opt-un [::specs/callback]))
 
-(defn get-layout
-  ([] (e/wrong-number-of-sdk-fn-args-err))
-  ([params & others]
-   (if-not (fn? (js->clj (first others)))
-     (e/wrong-number-of-sdk-fn-args-err)
-     (get-layout (merge (ih/extract-params params true) {:callback (first others)}))))
-  ([params]
-   (let [{:keys [layoutId callback] :as params} (ih/extract-params params true)
-         url {:base :single-layout-url
-              :params {:tenant-id (state/get-active-tenant-id)
-                       :layout-id layoutId}}
-         method :get]
-     (contact-request url nil method params :get-layout ::get-layout-params true))))
+(def-sdk-fn get-layout
+  {:validation ::get-layout-params
+   :topic-key :get-layout}
+  [params]
+  (let [{:keys [layout-id callback topic]} params
+        {:keys [api-response status]} (a/<! (rest/get-layout-request layout-id))
+        retrieved-layout (:result api-response)]
+    (when (= status 200)
+      (p/publish {:topics topic
+                  :response retrieved-layout
+                  :callback callback} true))))
+
+;; -------------------------------------------------------------------------- ;;
+;; CxEngage.contacts.listLayouts();
+;; -------------------------------------------------------------------------- ;;
 
 (s/def ::list-layouts-params
   (s/keys :req-un []
           :opt-un [::specs/callback]))
 
-(defn list-layouts
-  ([]
-   (list-layouts {}))
-  ([params & others]
-   (if-not (fn? (js->clj (first others)))
-     (e/wrong-number-of-sdk-fn-args-err)
-     (list-layouts (merge (ih/extract-params params true) {:callback (first others)}))))
-  ([params]
-   (let [params (if (fn? params) {:callback params} params)
-         {:keys [callback] :as params} (ih/extract-params params true)
-         url {:base :multiple-layout-url
-              :params {:tenant-id (state/get-active-tenant-id)}}
-         method :get]
-     (contact-request url nil method params :list-layouts ::list-layouts-params true))))
+(def-sdk-fn list-layouts
+  {:validation ::list-layouts-params
+   :topic-key :list-layouts}
+  [params]
+  (let [{:keys [callback topic]} params
+        {:keys [api-response status]} (a/<! (rest/list-layouts-request))
+        retrieved-layouts (:result api-response)]
+    (when (= status 200)
+      (p/publish {:topics topic
+                  :response retrieved-layouts
+                  :callback callback} true))))
 
 (defrecord ContactsModule []
   pr/SDKModule
