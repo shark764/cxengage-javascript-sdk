75992b439246a082da6b3d09fe48e952517606e1
Irvin Sandoval <irvandoval@.com>
2019-05-06 07:34:12 -0600

    CXV1-16547: Change metadata type property to agent only

--- src/cljs/cxengage_javascript_sdk/core.cljs
+++ src/cljs/cxengage_javascript_sdk/core.cljs
@@ -34,7 +34,7 @@
             [cxengage-javascript-sdk.modules.salesforce-lightning :as sfl]
             [cxengage-javascript-sdk.modules.testing :as testing]))
 
-(def *SDK-VERSION* "8.54.0")
+(def *SDK-VERSION* "8.54.1")
 
 (defn register-module
   "Registers a module & its API functions to the CxEngage global. Performs a deep-merge on the existing global with the values provided."
