0c18d5f20667066efcee5af831601d4b39f854ce
Josh Clowater <joshclowater@users.noreply.github.com>
2021-07-20 09:49:19 -0300

    Cut version for CXV1-24132 (#746)

--- src/cljs/cxengage_javascript_sdk/core.cljs
+++ src/cljs/cxengage_javascript_sdk/core.cljs
@@ -35,7 +35,7 @@
             [cxengage-javascript-sdk.modules.salesforce-lightning :as sfl]
             [cxengage-javascript-sdk.modules.testing :as testing]))
 
-(def *SDK-VERSION* "10.2.0-SNAPSHOT")
+(def *SDK-VERSION* "10.2.0")
 (defn register-module
   "Registers a module & its API functions to the CxEngage global. Performs a deep-merge on the existing global with the values provided."
   [module]
